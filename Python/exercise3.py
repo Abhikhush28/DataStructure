3f094918a9b011919533ec581fde1c036245a2d7
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-09-23 03:18:42 +0530

    Create exercise3.py

---
+++ Python/exercise3.py
@@ -0,0 +1,13 @@
+# Exercise 3: Slice list into 3 equal chunks and reverse each chunk
+# Given:
+
+# sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+
+# Expected Outcome:
+
+# Chunk  1 [11, 45, 8]
+# After reversing it  [8, 45, 11]
+# Chunk  2 [23, 14, 12]
+# After reversing it  [12, 14, 23]
+# Chunk  3 [78, 45, 89]
+# After reversing it  [89, 45, 78]
